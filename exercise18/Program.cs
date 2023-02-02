4d3990ef650ab3ef74f1318896c3cdc0f7b9801d
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-02 18:39:21 +0300

    Exercise 18 is completed

---
+++ exercise18/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Enter a quarter of XY-plot: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+if(quarter>0 && quarter<5)
+{
+    if (quarter == 1)
+    {
+        Console.WriteLine("Values of the selected quarter lie in range (X>0;Y>0)");
+    }
+    if (quarter == 2)
+    {
+        Console.WriteLine("Values of the selected quarter lie in range (X<0;Y>0)");
+    }
+    if (quarter == 3)
+    {
+        Console.WriteLine("Values of the selected quarter lie in range (X<0;Y<0)");
+    }
+    if (quarter == 4)
+    {
+        Console.WriteLine("Values of the selected quarter lie in range (X>0;Y<0)");
+    }
+}
+else
+{
+    Console.WriteLine("Enter a quarter from 1 to 4");
+}
