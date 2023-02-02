38f1f7063b18218fec8c8793294f2f3fcc9ce6fe
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-02 18:30:52 +0300

    Exercise 17 is completed

---
+++ exercise17/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Enter an X value:");
+int X = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter an Y value:");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if(X!=0 && Y!=0)
+{
+    if (X>0 && Y>0)
+    {
+        Console.WriteLine("Your point lies in the first quarter");
+    }
+    if (X>0 && Y<0)
+    {
+        Console.WriteLine("Your point lies in the fourth quarter");
+    }
+    if (X<0 && Y<0)
+    {
+        Console.WriteLine("Your point lies in the third quarter");
+    }
+    if (X<0 && Y>0)
+    {
+        Console.WriteLine("Your point lies in the second quarter");
+    }
+}
+else
+{
+    Console.WriteLine("Enter values are not equal to 0");
+}
