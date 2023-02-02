ba70dc1b2c9e707666a81affb03b308dec410ac5
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-02-02 19:36:46 +0300

    Exercise 21 & 22 are completed

---
+++ exercise21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter first X value: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter first Y value: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second X value: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second Y value: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double vectorLength = Math.Sqrt(Math.Pow((x1-x2),2)+Math.Pow((y1-y2),2));
+vectorLength = Math.Round(vectorLength,3);
+
+Console.Write(vectorLength);
---
+++ exercise22/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter an integer number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int count = 1; count <= N; count++)
+{
+    int result = Convert.ToInt32(Math.Pow(count,2));
+    Console.Write($"{result} ");
+}
